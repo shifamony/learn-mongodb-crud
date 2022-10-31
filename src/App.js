4d2dbd51386d2f652e1b3843eaa8708a4bacd30f
jhara19 <jharaislam639@gmail.com>
2022-10-31 12:51:32 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { toast } from 'react-toastify';
+import { RouterProvider } from 'react-router-dom';
+import router from './Routers/Routes/Routes';
 
 function App() {
+
+    const notify = () => toast("Wow so easy!");
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App max-w-7xl mx-auto">
+      <RouterProvider router={router}></RouterProvider>
+      <button className='btn btn-warning mt-4' onClick={notify}>Notify!</button>
     </div>
   );
 }
---
+++ src/Routers/Routes/Routes.js
@@ -0,0 +1,25 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../layout/Main";
+import Home from "../../Pages/Home/Home";
+
+
+const router = createBrowserRouter([
+   {
+     path: '/',
+     element: <Main></Main>,
+     children: [
+        {
+
+            path:'/',
+            element: <Home></Home>
+        },
+        {
+    
+            path:'/home',
+            element: <Home></Home>
+        },
+     ]
+}
+
+]);
+export default router;
